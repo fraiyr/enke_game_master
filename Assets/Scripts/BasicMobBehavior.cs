aae55dd818d94e401c32513278994bd7663ff0c3
chuck <chuck.coleman.00@gmail.com>
2016-04-21 21:04:15 -0400

    gitignore is working

---
+++ Assets/Scripts/BasicMobBehavior.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMobBehavior : MonoBehaviour {
+	public GameObject Enemy;
+
+	public Rigidbody2D rb;
+	public Animator anim;
+
+	public Rigidbody2D projectile;
+
+
+	int i = 1;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		projectile = GameObject.Find("Projectile").GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//framerate independent update
+	void FixedUpdate() {
+
+		
+		if (i == 1)
+		{
+			createProjectile();			
+		}
+
+		i++;
+
+		if (i > 50){
+			i = 1;
+		}
+		
+	}
+
+	void Pathing(){
+		
+
+
+	}
+
+	void createProjectile(){
+		Rigidbody2D clone; 
+		Vector3 position = new Vector3(rb.position.x - 0.06f, rb.position.y, 0);
+		clone = (Rigidbody2D)Instantiate(projectile, position, transform.rotation);
+        clone.velocity = new Vector2(-0.5f, 0);
+        Destroy(clone.gameObject, 5);
+	}
+}
---
+++ Assets/Scripts/CharacterFinal.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//This script makes the character move when the screen is pressed and handles the jump
+public class CharacterFinal : MonoBehaviour
+{
+    //Condition for whether the player should jump.
+    public bool jump = false;
+
+    //Amount of force added when the player jumps.
+    public float jumpForce = 10.0f;
+
+    //Whether or not the player is grounded.
+    public bool grounded = false;
+
+    //The vertical speed of movement.
+    public int movementSpeed = 10;
+
+    //The animator that controls the characters animations
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    //This method is called when the character collides with a collider (could be a platform).
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        grounded = true;
+        print("isground");
+    }
+
+    //The update method is called many times per second
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            //If the jump button is pressed and the player is grounded and the character is running forward then the player should jump.
+            if(grounded == true)
+            {
+                jump = true;
+                grounded = false;
+
+                //Trigger the Jump animation
+                anim.SetTrigger("Jump");
+            }
+        }
+    }
+
+    //Since we are using physics for movement, we use the FixedUpdate method
+    void FixedUpdate()
+    {
+        //if died
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        //else
+        //moving
+
+        //If jump is set to true, we add a quick force impulse for the jump
+        if(jump == true)
+        {
+            //Add a vertical force to the player
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+            //Set the variable to false again to avoid adding force constantly
+            jump = false;
+        }
+    }
+}
