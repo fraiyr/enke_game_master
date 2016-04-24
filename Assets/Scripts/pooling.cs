a45e3b1888ee3d8630fba35520a7fdcde2016a24
chuck <chuck.coleman.00@gmail.com>
2016-04-24 10:17:49 -0400

    added some comments

--- Assets/Scripts/pooling.cs
+++ Assets/Scripts/pooling.cs
@@ -4,45 +4,30 @@ using System.Collections;
 public class pooling : MonoBehaviour {
 
 	public GameObject otherBack;
-    public GameObject bat0;
-    public GameObject bat1;
     public Rigidbody2D player;
     public Rigidbody2D batObj;
     public BoxCollider2D sceneTrigger;
     
 
     
-
+    //Trigger happens when player exits collider box
     public void OnTriggerExit2D(Collider2D col)
     {
+        //Find the hero object
         player = GameObject.Find("Hero").GetComponent<Rigidbody2D>();
 
+        //Make sure the player is hitting the box, not an npc
         if (col.gameObject.tag == "Player")
         {
+            //This determines which way to move the level pieces so that they don't get split up
             if (player.velocity.x > 0 && (otherBack.transform.position.x < player.transform.position.x))
             {
                 Debug.Log("Character entered trigger");
 
-                System.Random random = new System.Random();
-                int spawnBats = random.Next(0, 2);
-
-                /*if (spawnBats == 0)
-                {
-                    createBat(14.266f);
-                }
-
-                else if (spawnBats == 1)
-                {
-                    createBat(19.266f);
-                }*/
-
-               
-                    //createBat(14.266f);
-                    //createBat(19.266f);
+                /*System.Random random = new System.Random();
+                int spawnBats = random.Next(0, 2);*/
                 
-
-
-
+                //Move the level section the character isn't in and create npc's
                 otherBack.transform.Translate(17.266f, 0, 0);
                 createBat(8.266f, .19f);
 
@@ -56,10 +41,14 @@ public class pooling : MonoBehaviour {
         }
     }
 
+    //Function to create npc's
     void createBat(float xLoc, float yLoc)
     {
+        //Look for the trigger to create npc's
         sceneTrigger = GameObject.Find("Trigger").GetComponent<BoxCollider2D>();
         Rigidbody2D batClone;
+
+        //create new bat objects relative to the trigger location
         Vector3 position = new Vector3((sceneTrigger.transform.position.x + xLoc), (sceneTrigger.transform.position.y + yLoc), 0);
 
         batClone = (Rigidbody2D)Instantiate(batObj, position, transform.rotation);
